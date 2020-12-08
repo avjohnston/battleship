8e7fd07d96e23111d7a8a01b84d8a37bd06c72ec
Andrew Johnston <av.johnst@gmail.com>
2020-12-07 18:00:01 -0700

    Start cell class

---
+++ lib/cell.rb
@@ -0,0 +1,35 @@
+require './lib/ship'
+
+class Cell
+  attr_reader :coordinate
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = []
+  end
+
+  def empty?
+    return true if @ship.length == 0
+    false
+  end
+
+  def place_ship(cruiser)
+    @ship << cruiser
+  end
+
+  def ship
+    @ship
+  end
+
+  def fired_upon?
+    return false if @health == @length
+    true
+  end
+
+  def fire_upon
+    # if @ship.length != 0
+    #
+    # else
+    # end
+  end
+
+end
---
+++ test/cell_test.rb
@@ -0,0 +1,59 @@
+require './lib/ship'
+require './lib/cell'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class CellTest < MiniTest::Test
+  def test_it_exists
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+  end
+
+  def test_it_has_a_coordinate
+    cell = Cell.new("B4")
+    assert_equal "B4", cell.coordinate
+  end
+
+  def test_it_has_a_ship
+    skip
+    cell = Cell.new("B4")
+    assert_equal nil, cell.ship
+  end
+
+  def test_the_cell_is_empty
+    cell = Cell.new("B4")
+    assert_equal true, cell.empty?
+  end
+
+  def test_ship_is_placed
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal [cruiser], cell.ship
+  end
+
+  def test_cell_isnt_empty
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal false, cell.empty?
+  end
+
+  def test_cell_can_be_fired_upon
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal false, cell.fired_upon?
+  end
+
+  def test_we_can_fire_upon_a_cell
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    cell.fire_upon
+    assert_equal 2, cruiser.health
+    assert_equal true, cell.fired_upon?
+  end
+
+
+end
