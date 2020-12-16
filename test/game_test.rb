ccafc5eb56ef5f663aca39e95eb6d2ebdfb354f7
Andrew Johnston <av.johnst@gmail.com>
2020-12-15 17:18:59 -0700

    Add test for computer coordinates

--- test/game_test.rb
+++ test/game_test.rb
@@ -10,4 +10,17 @@ class GameTest < MiniTest::Test
     game = Game.new
     assert_instance_of Game, game
   end
+
+  def test_computer_coordinates
+    game = Game.new
+    player = Board.new
+    computer = Board.new
+    computer_submarine = Ship.new("Submarine", 2)
+    user_submarine = Ship.new("Submarine", 2)
+    computer_cruiser = Ship.new("Cruiser", 3)
+    user_cruiser = Ship.new("Cruiser", 3)
+    expected = game.random_coordinate_generator(computer_submarine)
+    assert_equal true, computer.valid_placement?(computer_submarine, expected)
+  end
+
 end
