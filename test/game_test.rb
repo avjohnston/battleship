8ac8c6c8080185e71d037e47ad66805dea71784e
Arika Turing <ablenker@gmail.com>
2020-12-15 17:16:47 -0700

    Work on game tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,10 +4,25 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/game'
+require 'mocha/minitest'
+
 
 class GameTest < MiniTest::Test
   def test_it_exists
     game = Game.new
     assert_instance_of Game, game
   end
+
+  def test_computer_coordinates
+    game = Game.new
+    player = Board.new
+    computer = Board.new
+    computer_submarine = Ship.new("Submarine", 2)
+    user_submarine = Ship.new("Submarine", 2)
+    computer_cruiser = Ship.new("Cruiser", 3)
+    user_cruiser = Ship.new("Cruiser", 3)
+    expected = game.random_coordinate_generator(computer_submarine)
+    assert_equal  true, computer.valid_placement?(computer_submarine, expected)
+  end
+
 end
